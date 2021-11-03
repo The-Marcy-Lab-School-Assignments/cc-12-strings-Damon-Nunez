64104eed4490ef6f49995aeae7eb588642174a30
Damon-Nunez <damon.nunez@student.uncommonhs.org>
2021-11-03 15:08:19 +0000

    o

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 //write your code here
+function reverseString(str) {
+    // Step 1. Create an empty string that will host the new created string
+    var newString = "";
+    for (var i = str.length - 1; i >= 0; i--) { 
+        newString += str[i]; 
+        
+    }
+    console.log(newString)
+}
+reverseString("Attempt")
